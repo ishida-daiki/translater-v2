a36e7b7450c6a7ab28da77c728546fcc00b4f21e
ishida-daiki <ishida-daiki@dmm.com>
2024-05-01 13:40:34 +0900

    Dropdown コンポーネントを追加

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,69 +1,107 @@
-import '!prismjs/themes/prism.css'
+import "!prismjs/themes/prism.css";
 
 import {
   Button,
   Container,
+  Dropdown,
+  DropdownOption,
   render,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h, RefObject } from 'preact'
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
+  SearchTextbox,
+  SelectableItem,
+  VerticalSpace,
+} from "@create-figma-plugin/ui";
+import { emit } from "@create-figma-plugin/utilities";
+import { h, JSX, RefObject } from "preact";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
+import { highlight, languages } from "prismjs";
+import Editor from "react-simple-code-editor";
 
-import styles from './styles.css'
-import { InsertCodeHandler } from './types'
+import styles from "./styles.css";
+import { InsertCodeHandler } from "./types";
 
 function Plugin() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
-  const containerElementRef : RefObject<HTMLDivElement> = useRef(null)
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+  const containerElementRef: RefObject<HTMLDivElement> = useRef(null);
   const handleInsertCodeButtonClick = useCallback(
     function () {
-      emit<InsertCodeHandler>('INSERT_CODE', code)
+      console.log("Hello");
+      // emit<InsertCodeHandler>('INSERT_CODE', code)
     },
     [code]
-  )
+  );
   // Patch to make `react-simple-code-editor` compatible with Preact
-  useEffect(function () {
-    const containerElement = containerElementRef.current
-    if (containerElement === null) {
-      return
-    }
-    const textAreaElement = containerElement.querySelector('textarea')
-    if (textAreaElement === null) {
-      return
-    }
-    textAreaElement.textContent = code
-    const preElement = containerElement.querySelector('pre')
-    if (preElement === null) {
-      return
-    }
-    if (textAreaElement.nextElementSibling !== preElement) {
-      textAreaElement.after(preElement)
-    }
-  }, [code])
+  useEffect(
+    function () {
+      const containerElement = containerElementRef.current;
+      if (containerElement === null) {
+        return;
+      }
+      const textAreaElement = containerElement.querySelector("textarea");
+      if (textAreaElement === null) {
+        return;
+      }
+      textAreaElement.textContent = code;
+      const preElement = containerElement.querySelector("pre");
+      if (preElement === null) {
+        return;
+      }
+      if (textAreaElement.nextElementSibling !== preElement) {
+        textAreaElement.after(preElement);
+      }
+    },
+    [code]
+  );
+  const [value, setValue] = useState<null | string>(null);
+  const options: Array<DropdownOption> = [
+    {
+      value: "English",
+    },
+    {
+      value: "Japanese",
+    },
+    {
+      value: "Spanish",
+    },
+    {
+      value: "French",
+    },
+    {
+      value: "German",
+    },
+    {
+      value: "Italian",
+    },
+    {
+      value: "Dutch",
+    },
+    {
+      value: "Polish",
+    },
+    "-",
+    {
+      header: "Header",
+    },
+    {
+      value: "qux",
+    },
+  ];
+  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
+    const newValue = event.currentTarget.value;
+    console.log(newValue);
+    setValue(newValue);
+  }
+
   return (
     <Container space="medium">
+      <VerticalSpace space="medium" />
+      <Dropdown onChange={handleChange} options={options} value={value} />
       <VerticalSpace space="small" />
-      <div class={styles.container} ref={containerElementRef}>
-        <Editor
-          highlight={function (code: string) {
-            return highlight(code, languages.js, 'js')
-          }}
-          onValueChange={setCode}
-          preClassName={styles.editor}
-          textareaClassName={styles.editor}
-          value={code}
-        />
-      </div>
-      <VerticalSpace space="large" />
       <Button fullWidth onClick={handleInsertCodeButtonClick}>
-        Insert Code
+        Translate
       </Button>
       <VerticalSpace space="small" />
     </Container>
-  )
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
