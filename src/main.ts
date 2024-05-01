108ca9a7dfeaac1582e1661214b086543d5f2480
ishida-daiki <ishida-daiki@dmm.com>
2024-05-01 17:45:52 +0900

    細かな設定

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
-import { loadFontsAsync, once, showUI } from '@create-figma-plugin/utilities'
-
-import { InsertCodeHandler } from './types'
+import { loadFontsAsync, once, showUI } from '@create-figma-plugin/utilities';
+import { InsertCodeHandler } from './types';
 
 export default function () {
   figma.ui.onmessage = async (msg) => {
     if (msg.type === "load") {
-      console.log("cool");
+      const authKey = 'API_KEY';
+      const textToTranslate = msg.text;
+      const targetLang = 'FR'; // ここで目的の言語を設定
+
+      // fetch APIを使用してDeepL APIへとリクエストを送信
+      try {
+        const response = await fetch('https://api-free.deepl.com/v2/translate', {
+          method: 'POST',
+          headers: {
+            'Authorization': `DeepL-Auth-Key ${authKey}`, // セキュアな場所にキーを格納する
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: `text=${encodeURIComponent(textToTranslate)}&target_lang=${targetLang}`
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data.translations[0].text); // 翻訳結果を表示
+      } catch (error) {
+        console.error('Translation error:', error);
+      }
     }
   }
   once<InsertCodeHandler>('INSERT_CODE', async function (code: string) {
